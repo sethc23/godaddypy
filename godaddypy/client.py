5e7e25dcc64daa85d3687f75f2ccaa62886fe5d1
sethc23 <sethc23@users.noreply.github.com>
2017-01-13 13:11:46 -0500

    drafted preliminary registry record handling upgrade

--- godaddypy/client.py
+++ godaddypy/client.py
@@ -9,6 +9,9 @@ class Client(object):
     """The GoDaddyPy Client.
 
     This client is used to connect to the GoDaddy API and to perform requests with said API.
+    
+    More Info:  https://developer.godaddy.com/ 
+
     """
     logging.basicConfig(filemode='a',
                         level=logging.INFO)
@@ -21,6 +24,11 @@ class Client(object):
         """
 
         self.API_TEMPLATE = 'https://api.godaddy.com/v1'
+
+        """
+        {type}:  [ 'A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'SRV', 'TXT' ]
+        {name}:  ( the subdomain name )
+        """
 
         self.GET_DOMAINS = '/domains'
         self.GET_DOMAIN = '/domains/{domain}'
@@ -92,19 +100,40 @@ class Client(object):
         url = self.API_TEMPLATE + self.GET_DOMAIN.format(domain=domain)
         return self._get(url, method_name=self.get_domain_info.__name__, headers=self._get_headers()).json()
 
-    def get_a_records(self, domain):
-        url = self.API_TEMPLATE + self.GET_RECORDS_TYPE_NAME.format(domain=domain, type='A')
-        data = self._get(url, method_name=self.get_a_records.__name__, headers=self._get_headers()).json()
+    def chk_rec_type(self, record_type):
+        possible_types = [ 'A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'SRV', 'TXT' ]
+        assert possible_types.count(record_type.upper()),'\n\nUnknown record type: "%s"; Expected one of %s\n\n' % (record_type.upper(),str(possible_types))
+
+    def get_records(self, domain, record_type=None):
+
+        self.chk_rec_type(record_type)
+        url = self.API_TEMPLATE + self.GET_RECORDS_TYPE_NAME.format(domain=domain, type=record_type)
+        data = self._get(url, method_name='get_%s_records' % record_type.lower(), headers=self._get_headers()).json()
 
         logging.info('Retrieved {} records from {}.'.format(len(data), domain))
 
         return data
 
-    def put_a_records(self, domain, records):
+    def put_records(self, domain, records):
         for _rec in records:
-            url = self.API_TEMPLATE + self.PUT_RECORDS_TYPE_NAME.format(domain=domain, type='A', name=_rec['name'])
-            self._put(url, json=_rec, method_name=self.get_a_records.__name__, headers=self._get_headers())
+            self.chk_rec_type(_rec['type'])
+            url = self.API_TEMPLATE + self.PUT_RECORDS_TYPE_NAME.format(domain=domain, type=_rec['type'], name=_rec['name'])
+            self._put(url, json=_rec, method_name='get_%s_records' % _rec['type'].lower(), headers=self._get_headers())
             logging.info('Updated {} records @ {}'.format(len(records), domain))
+
+    # def get_a_records(self, domain):
+    #     url = self.API_TEMPLATE + self.GET_RECORDS_TYPE_NAME.format(domain=domain, type='A')
+    #     data = self._get(url, method_name=self.get_a_records.__name__, headers=self._get_headers()).json()
+
+    #     logging.info('Retrieved {} records from {}.'.format(len(data), domain))
+
+    #     return data
+
+    # def put_a_records(self, domain, records):
+    #     for _rec in records:
+    #         url = self.API_TEMPLATE + self.PUT_RECORDS_TYPE_NAME.format(domain=domain, type='A', name=_rec['name'])
+    #         self._put(url, json=_rec, method_name=self.get_a_records.__name__, headers=self._get_headers())
+    #         logging.info('Updated {} records @ {}'.format(len(records), domain))
 
     def update_ip(self, ip, domains=None, subdomains=None):
         """Update the IP address in all A records to the value of ip.  Returns True if no exceptions occurred during
@@ -129,7 +158,8 @@ class Client(object):
             raise SystemError("Domains must be type 'list' or type 'str'")
 
         for domain in domains:
-            records = self.get_a_records(domain)
+            import ipdb as I; I.set_trace()
+            records = self.get_records(domain,'A')
             new_records = []
             for record in records:
                 r_name = str(record['name'])
@@ -152,7 +182,7 @@ class Client(object):
 
                         new_records.append(record)
 
-            self.put_a_records(domain, new_records)
+            self.put_records(domain, new_records)
 
         # If we didn't get any exceptions, return True to let the user know
         return True
